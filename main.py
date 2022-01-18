b5c89cd75244579c56fd07fc5166f64cea59ae69
Tuqa Aburaddaha <80428116+Tuqa13@users.noreply.github.com>
2022-01-18 12:08:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,68 +2,28 @@ import requests
 from twilio.rest import Client
 
 
-account_sid = "ACf832560790170b91445f261a4e02f057"
-auth_token = "6f1b173f71a1af41e44cf7ecbeb8d2dc"
+account_sid = "ACf83256079017*********e02f057"
+auth_token = "6f1b173f71a*********beb8d2dc"
 
 parameters = {
-    "lat": 31.945368 ,
+    "lat": 31.945368 , // Amman location
     "lon": 35.928371,
     "exclude": "current,minutely,daily",
-    "appid": "8cf0d29e8a80d55e18a11b893d131e96",
+    "appid": "8cf0d29e8a80d******3d131e96",
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
-data = response.json()["hourly"][:12]
+data = response.json()["hourly"][:12] // To check all the hours during the day
 for hour_data in data:
-    if hour_data["weather"][0]["id"] > 700:
+    if hour_data["weather"][0]["id"] > 700: // if the id > 700, then it will rain
         client = Client(account_sid, auth_token)
         message = client.messages.create(
             body="It will rain today!\nMake sure to bring your Umbrella ☂️☂️",
-            from_="+12244790631",
-            to="+962786790258"
+            from_="+122*****31", 
+            to="+9627*****258"
         )
 
         print(message.status)
         break
 
-
-# import requests
-# import os
-# from twilio.rest import Client
-# from twilio.http.http_client import TwilioHttpClient
-#
-#
-# account_sid = "ACf832560790170b91445f261a4e02f057"
-# auth_token = "6f1b173f71a1af41e44cf7ecbeb8d2dc"
-#
-# parameters = {
-#     "lat": 31.945368 ,
-#     "lon": 35.928371,
-#     "exclude": "current,minutely,daily",
-#     "appid": "8cf0d29e8a80d55e18a11b893d131e96",
-# }
-#
-# response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
-# response.raise_for_status()
-# data = response.json()["hourly"][:12]
-# for hour_data in data:
-#     if hour_data["weather"][0]["id"] > 700:
-#         proxy_client = TwilioHttpClient()
-#         proxy_client.session.proxies = {'https': os.environ['https_proxy']}
-#         client = Client(account_sid, auth_token, http_client=proxy_client)
-#         message1 = client.messages.create(
-#             body="It Will Rain Today!\nMake Sure to Bring your Umbrella ☂️☂️",
-#             from_="+12244790631",
-#             to="+962786790258"
-#         )
-#
-#         message2 = client.messages.create(
-#             body="It Will Rain Today!\nMake Sure to Bring your Umbrella ☂️☂️",
-#             from_="+12244790631",
-#             to="+962790296658"
-#         )
-#
-#         print(message1.status)
-#         print(message2.status)
-#         break
